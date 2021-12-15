4fd49698561545fbed0fe02b23f6b2fc2ac832ad
Andy Luu <andyluu324@gmail.com>
2021-12-15 17:02:39 -0500

    Add files via upload

---
+++ 2017 Lockheed CodeQuest Problems/Problem1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Problem1 {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(new File("2017 Lockheed CodeQuest Problems/ExampleInputs/Prob01.in.txt"));
+		int cases = in.nextInt();
+		in.nextLine();
+		for (int a = 0; a < cases; a++) {
+			String line = in.nextLine();
+			System.out.println(line);
+			System.out.println(line);
+		}
+	}
+}
---
+++ 2017 Lockheed CodeQuest Problems/Problem18.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Problem18 {
+	public static void main(String[] args) {
+
+	}
+}
